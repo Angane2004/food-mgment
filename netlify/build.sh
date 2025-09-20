f8b04e25032a6f144236d66015d716b1180a7426
Angane2004 <anganetanveer@gmail.com>
2025-09-21 00:14:25 +0530

    UI Updated

--- netlify/build.sh
+++ netlify/build.sh
@@ -3,8 +3,8 @@
 # Force Node.js version
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 20.11.0
-nvm use 20.11.0
+nvm install 20
+nvm use 20
 
 # Continue with the build
 npm run build
